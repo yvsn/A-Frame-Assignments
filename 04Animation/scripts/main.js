d09e6c4b14a9e84844425fa700e0c84052d069c3
yvsn <yvaessen@hotmail.de>
2021-05-13 12:58:35 +0200

    Assignment 4 (Animation) part 7

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,12 +1,16 @@
 
 
 // // changing individual properties with code and using setInterval
-// var rotationSpeed = 0.01;
-// var myOtherBox = document.getElementById('myOtherBox');
+var rotationSpeed = -0.05;
+var rotationSpeed_2 = 0.02;
+var myOtherBox = document.getElementById('myOtherBox');
 
-// function spin(){
-// 	myOtherBox.object3D.rotation.x += rotationSpeed;
-// 	console.log(myOtherBox.object3D.rotation.x);
-// }
-
-// setInterval(spin, 16); //equivalent to 60 fps
+function spin(){
+myOtherBox.object3D.rotation.x += rotationSpeed;
+myOtherBox.object3D.rotation.y += rotationSpeed_2;
+myOtherBox.object3D.location.z += rotationSpeed;
+console.log(myOtherBox.object3D.rotation.x);
+console.log(myOtherBox.object3D.rotation.y);
+console.log(myOtherBox.object3D.location.z);
+}
+setInterval(spin, 16); //equivalent to 60 fps
